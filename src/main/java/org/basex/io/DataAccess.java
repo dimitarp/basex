312f1936bd430fb34082bccfb0409b09e0d39020
dimitarp <dimitar.popov@uni-konstanz.de>
2011-04-08 18:27:45 +0200

    [FIX] Fixed a comment of DataAccess.pos()

--- src/main/java/org/basex/io/DataAccess.java
+++ src/main/java/org/basex/io/DataAccess.java
@@ -53,7 +53,7 @@ public final class DataAccess {
 
   /**
    * Returns the current file position.
-   * @return text as byte array
+   * @return position in the file
    */
   public long pos() {
     return bm.current().pos + off;
